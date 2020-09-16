de30c46b10525f959618b2f7d3dab10dd8b6a307
Laptop <tgoodell@disroot.org>
2020-09-16 07:31:26 -0500

    31

--- A02/program.py
+++ A02/program.py
@@ -10,18 +10,12 @@ def show(img,wait=0,destroy=True):
 
 img1=cv2.imread("input/image1.png")
 img2=cv2.imread("input/image2.png")
-cube=cv2.imread("input/cube.png")
-book=cv2.imread("input/book.png")
+cube=cv2.imread("input/image3.png")
+biscuits=cv2.imread("input/image4.png")
 h1,w1=img1.shape[:2]
 h2,w2=img1.shape[:2]
 ch,cw=cube.shape[:2]
 bh,bw=cube.shape[:2]
-
-angle=math.pi*30/180
-T1=np.float64([[1,0,-w1/2],[0,1,-h1/2],[0,0,1]])
-T2=np.float64([[1,0,w1/2],[0,1,h1/2],[0,0,1]])
-T4=np.float64([[1,0,100],[-100,1,0],[0,0,1]])
-R=np.float64([[np.cos(angle),-np.sin(angle),0],[np.sin(angle),np.cos(angle),0],[0,0,1]])
 
 # 1 - Mirror Across Y-Axis
 mirror=np.float64([[-1,0,w1],[0,1,0],[0,0,1]])
@@ -52,28 +46,12 @@ newWidth=np.uint64(side2+side3)
 
 flipy=np.float64([[-1,0,w1],[0,1,0],[0,0,1]])
 rotate30=np.float64([[-np.cos(angle),np.sin(angle),0],[np.sin(angle),np.cos(angle),0],[0,0,1]])
-left=np.float64([[1,0,300],[0,1,0],[0,0,1]])
-final=flipy@rotate30@flipy@left
-final = cv2.warpPerspective(img1,final,dsize=(newHeight,newWidth))
+modify=np.float64([[1,0,250],[0,1,0],[0,0,1]])
+final=flipy@modify@rotate30@flipy
+final = cv2.warpPerspective(img1,final,(newWidth,newHeight))
 
 show(final)
 cv2.imwrite("output/pic_1_3.png",final)
-
-
-pts1 = np.float32([[124,455],[547,349],[883,781],[393,979]])
-pts2 = np.float32([[0,0],[bw,0],[bw,bh],[0,bh]])
-
-# Apply Perspective Transform Algorithm
-M = cv2.getPerspectiveTransform(pts1, pts2)
-result=cv2.warpPerspective(book,M,(w1,h1))
-
-# ~ print(M@np.float64([[0],[0],[1]]))
-#tp=cv2.perspectiveTransform(srcPoints,dstPoints)
-show(result)
-#print(tp)
-#maxX,maxY=tp.max(axis=1)[0]
-#minX,minY=tp.min(axis=1)[0]
-#print(minX,maxX,minY,maxY)
 
 # Cube w/ Faces
 # First Face
@@ -107,3 +85,13 @@ p=blankPerspective/255.0*.8
 cube=np.uint8(squirrelPerspective*p+cube*(1-p))
 show(cube)
 cv2.imwrite("output/pic_1_4.png",cube)
+
+# 5 Flatten image4
+
+pts1 = np.float32([[158,544],[853,431],[791,764],[212,994]])
+pts2 = np.float32([[0,0],[bw,0],[bw,bh],[0,bh]])
+
+M = cv2.getPerspectiveTransform(pts1, pts2)
+result=cv2.warpPerspective(biscuits,M,(1000,670))
+cv2.imwrite("output/pic_1_5.png",result)
+show(result)
